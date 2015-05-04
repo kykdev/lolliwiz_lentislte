363cd6fbaff62d8b0e892931d89a8608ccbb5bc8
kykdev <kykint@naver.com>
2015-05-04 12:48:06 +0900

    Use prebuilt ramdisk for now
    Signed-off-by: kykdev <kykint@naver.com>
    (cherry picked from commit ca44b74cd473f4b2f1bea08e4992e6f5538f4cb1)
    Signed-off-by: kykdev <kykint@naver.com>

--- ramdisk/lgu/init.lw.sh
+++
@@ -1,9 +0,0 @@
-#!/system/bin/sh
-
-# Enable Sweep2Sleep
-echo 1 > /sys/sweep2sleep/sweep2sleep
-
-# Disable securestorage support
-mount -o rw,remount /system
-sed -i 's/ro.securestorage.support=true/ro.securestorage.support=false/' /system/build.prop
-
