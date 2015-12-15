53828eaa7aa1c9621f6fa02fd0ed5e5bb12d8941
kykdev <kykint@naver.com>
2015-12-15 14:03:04 +0900

    Revert "Revert back to G906SKSU1BOB8"
    This reverts commit d50b6171b2a063a63926784674a27df28f5eebf9.

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -249,6 +249,12 @@ static DEFINE_MUTEX(scan_mutex);
 #define SSWAP_LMK_THRESHOLD	(30720 * 2)
 #define CMA_PAGE_RATIO		70
 #endif
+
+#if defined(CONFIG_ZSWAP)
+extern atomic_t zswap_pool_pages;
+extern atomic_t zswap_stored_pages;
+#endif
+
 static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 {
 	struct task_struct *tsk;
@@ -269,6 +275,7 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 	static DEFINE_RATELIMIT_STATE(lmk_rs, DEFAULT_RATELIMIT_INTERVAL, 1);
 #endif
 	unsigned long nr_cma_free;
+	struct reclaim_state *reclaim_state = current->reclaim_state;
 #if defined(CONFIG_CMA_PAGE_COUNTING)
 	unsigned long nr_cma_inactive_file;
 	unsigned long nr_cma_active_file;
@@ -377,6 +384,15 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 			continue;
 		}
 		tasksize = get_mm_rss(p->mm);
+#if defined(CONFIG_ZSWAP)
+		if (atomic_read(&zswap_stored_pages)) {
+			lowmem_print(3, "shown tasksize : %d\n", tasksize);
+			tasksize += atomic_read(&zswap_pool_pages) * get_mm_counter(p->mm, MM_SWAPENTS)
+				/ atomic_read(&zswap_stored_pages);
+			lowmem_print(3, "real tasksize : %d\n", tasksize);
+		}
+#endif
+
 		task_unlock(p);
 		if (tasksize <= 0)
 			continue;
@@ -448,6 +464,8 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 #endif
 		/* give the system time to free up the memory */
 		msleep_interruptible(20);
+		if(reclaim_state)
+			reclaim_state->reclaimed_slab = selected_tasksize;
 		trace_almk_shrink(selected_tasksize, ret,
 			other_free, other_file, selected_oom_score_adj);
 	} else {
